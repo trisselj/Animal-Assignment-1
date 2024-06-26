d8dad7daeadd79f4123ef20b154ff3ae0c552956
trisselj <trisselj@oregonstate.edu>
2024-06-26 13:06:34 -0700

    Update

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Jake Trissel
+# Github Username: trisselj
+# Date: 06/26/2024
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal!
+
+fave_animal = input("What is your favorite animal?")
+print("Then that must mean your favorite animal is the " + fave_animal + ".")
